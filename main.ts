52501c19ae5b6b4ca11e9647b2ffcaf68cc3836a
Dan Stormont <daniel.stormont@gmail.com>
2022-11-02 16:33:00 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    info.changeScoreBy(1)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    info.changeScoreBy(1)
+})
+info.setScore(0)
+info.startCountdown(10)
